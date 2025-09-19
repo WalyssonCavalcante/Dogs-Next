07780164663f3b414e079a7e5cbc25a7f056a806
Wallysson Cavalcante <wallysson.cavalcante@gmail.com>
2025-09-18 22:11:18 -0300

    Feat: Criação do Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { type_second } from "@/functions/fonts";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 export const metadata: Metadata = {
   title: "Dogs",
@@ -16,7 +17,8 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <Header />
-      <body className={type_second.variable}>{children}</body>
+      <body>{children}</body>
+      <Footer />
     </html>
   );
 }
